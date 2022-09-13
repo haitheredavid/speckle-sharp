6b47099f5af4f20d24453662638584dbfa669069
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-09-13 18:01:45 -0400

    setup for Bundle.cs

---
+++ Objects/Objects/Organization/Bundle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Speckle.Core.Models;
+
+namespace Objects.Organization
+{
+	/// <summary>
+	/// A simple organization object that links multiple speckle objects into one bundle.
+	/// All linked objects must be situated on the same server
+	/// </summary>
+	public class Bundle : Base
+	{
+		/// <summary>
+		/// Name of the bundle
+		/// </summary>
+		public string name { get; set; }
+
+		/// <summary>
+		/// A list of reference object Ids to link together
+		/// </summary>
+		[DetachProperty] public List<string> referenceObjects { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Bundle()
+		{ }
+
+		/// <summary>
+		/// Basic constructor 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="referenceObjects"></param>
+		public Bundle(string name, List<string> referenceObjects)
+		{
+			this.name = name;
+			this.referenceObjects = referenceObjects;
+		}
+
+	}
+}
