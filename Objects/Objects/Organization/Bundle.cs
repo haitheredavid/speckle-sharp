705a1736d777eba741676bd24f91dcb75faaa018
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-09-13 20:14:39 -0400

    changed bundle to contain stream ID

--- Objects/Objects/Organization/Bundle.cs
+++ Objects/Objects/Organization/Bundle.cs
@@ -16,8 +16,9 @@ namespace Objects.Organization
 
 		/// <summary>
 		/// A list of reference object Ids to link together
+		/// Collection key is the streamId associated with the list of objects to use 
 		/// </summary>
-		[DetachProperty] public List<string> referenceObjects { get; set; }
+		[DetachProperty] public Dictionary<string, List<string>> referenceObjects { get; set; }
 
 		/// <summary>
 		/// 
@@ -30,11 +31,40 @@ namespace Objects.Organization
 		/// </summary>
 		/// <param name="name"></param>
 		/// <param name="referenceObjects"></param>
-		public Bundle(string name, List<string> referenceObjects)
+		public Bundle(string name, Dictionary<string, List<string>> referenceObjects)
 		{
 			this.name = name;
 			this.referenceObjects = referenceObjects;
 		}
 
+		/// <summary>
+		/// Checks if <see cref="name"/> and <see cref="referenceObjects"/> are similar
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public bool Equals(Bundle obj)
+		{
+			// check if name are similar along with collection of objects
+			if (!this.name.Equals(obj.name) || obj.referenceObjects?.Count != this.referenceObjects?.Count)
+				return false;
+
+			foreach (var key in this.referenceObjects.Keys)
+			{
+				var ids = this.referenceObjects[key];
+
+				// check that ids exists and are similar in size 
+				if (!obj.referenceObjects.TryGetValue(key, out var objIds) || ids.Count != objIds.Count)
+					return false;
+
+				// check all ids
+				for (int i = 0; i < ids.Count; i++)
+				{
+					if (!ids[i].Equals(objIds[i]))
+						return false;
+				}
+			}
+
+			return true;
+		}
 	}
 }
